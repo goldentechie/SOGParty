19948811e5cd5ff8dc7b6a0e5de7c762f4f3c938
goldentechie <bendam1207@gmail.com>
2014-04-13 19:42:07 +0000

    fix for validation on increased block expiration time

--- src/js/components/balances.js
+++ src/js/components/balances.js
@@ -122,10 +122,11 @@ function CreateNewAddressModalViewModel() {
     var newAddress = null;
 
     if(!self.forWatchOnly()) {
-      newAddress = WALLET.BITCOIN_WALLET.generateAddress();
-      var i = WALLET.BITCOIN_WALLET.addresses.length - 1;
-      var privkey = WALLET.BITCOIN_WALLET.getPrivateKey(i);
-      WALLET.addAddress(privkey);
+      WALLET.BITCOIN_WALLET.generateAddress();
+      var i = WALLET.BITCOIN_WALLET.getPrivateKeys().length - 1;
+      var hd = WALLET.BITCOIN_WALLET.getPrivateKey(i);
+      newAddress = hd.priv.getBitcoinAddress().toString();
+      WALLET.addAddress(hd.priv);
     } else {
       newAddress = self.watchAddress();
       WALLET.addWatchOnlyAddress(newAddress);
@@ -298,12 +299,16 @@ function SweepModalViewModel() {
     required: true,
     validation: {
       validator: function (val, self) {
-        var key = new Bitcoin.ECKey(self.privateKey());
-        
-        var doesVersionMatch = key.version == (USE_TESTNET ?
-          Bitcoin.network.testnet.addressVersion : Bitcoin.network.mainnet.addressVersion);
+  
+        var key = BitcoinECKey(self.privateKey());      
+        var doesVersionMatch = key.version == NETWORK_VERSION;
 
-        return isWifKey(self.privateKey()) && key.priv !== null && key.compressed !== null && key.version !== null && doesVersionMatch;
+        /*$.jqlog.debug('adress:'+key.getBitcoinAddress());
+        $.jqlog.debug('compressed:'+key.compressed);
+        $.jqlog.debug('version:'+key.version);
+        $.jqlog.debug('priv:'+key.priv);*/
+
+        return key.priv !== null && key.compressed !== null && key.version !== null && doesVersionMatch;
       },
       message: 'Not a valid' + (USE_TESTNET ? ' TESTNET ' : ' ') + 'private key.',
       params: self
@@ -337,9 +342,9 @@ function SweepModalViewModel() {
   self.addressForPrivateKey = ko.computed(function() {
     if(!self.privateKeyValidated.isValid()) return null;
     //Get the address for this privatekey
-    var key = new Bitcoin.ECKey(self.privateKey());
+    var key = BitcoinECKey(self.privateKey());
     assert(key.priv !== null && key.compressed !== null, "Private key not valid!"); //should have been checked already
-    return key.getAddress(NETWORK_VERSION).toString();
+    return key.getBitcoinAddress().toString();
   }, self);
   self.numPrimedTxoutsForPrivateKey = ko.observable(null);
   
@@ -406,7 +411,7 @@ function SweepModalViewModel() {
   self._signInputs = function(unsignedTxHex) {
     var sendTx = Bitcoin.Transaction.deserialize(unsignedTxHex);
     var txInHash = null, signature = null, SIGHASH_ALL = 1;
-    var key = new Bitcoin.ECKey(self.privateKey());
+    var key = new BitcoinECKey(self.privateKey());
     for(var i = 0; i < sendTx.ins.length; i++) {
       txInHash = txIn.hashTransactionForSignature(sendTx.ins[i].script, i, SIGHASH_ALL);
       signature = key.sign(txInHash);
@@ -561,7 +566,7 @@ function SweepModalViewModel() {
   }
   
   self.doAction = function() {
-    var key = new Bitcoin.ECKey(self.privateKey());
+    var key = new BitcoinECKey(self.privateKey());
     assert(key.priv !== null && key.compressed !== null, "Private key not valid!"); //should have been checked already
     var pubkey = key.getPub().toHex();
     var sendsToMake = [];
@@ -700,10 +705,9 @@ function SignMessageModalViewModel() {
   self.doAction = function() {
     assert(self.validationModel.isValid(), "Cannot sign");
     var key = WALLET.getAddressObj(self.address()).KEY;
-    var hexSignedMessage = Bitcoin.Message.sign(key, self.message());
-    $.jqlog.debug('hexSignedMessage: '+hexSignedMessage);
+    var hexSignedMessage = Bitcoin.Message.signMessage(key, self.message(), key.compressed);
     self.signedMessage(self.signatureFormat() == 'base64'
-      ? Bitcoin.convert.bytesToBase64(hexSignedMessage) : Bitcoin.convert.bytesToHex(hexSignedMessage));
+      ? Bitcoin.convert.bytesToBase64(Bitcoin.convert.hexToBytes(hexSignedMessage)) : hexSignedMessage);
     $("#signedMessage").effect("highlight", {}, 1500);
     //Keep the form up after signing, the user will manually press Close to close it...
   }
