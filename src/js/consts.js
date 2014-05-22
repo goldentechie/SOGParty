cdf7d84ba2cc315d42e70b45aa29c8e34967bd03
goldentechie <bendam1207@gmail.com>
2014-05-22 10:22:07 +0000

    fixed on screen keyboard not working

--- src/js/consts.js
+++ src/js/consts.js
@@ -49,7 +49,8 @@ var DEFAULT_PREFERENCES = {
   'selected_theme': 'ultraLight',
   'selected_lang': 'en-us',
   'watch_only_addresses': [],
-  'auto_btcpay': true //default to auto BTC payments being enabled
+  'auto_btcpay': true, //default to auto BTC payments being enabled
+  'has_accepted_license': false
 };
 
 var ENTITY_NAMES = {
@@ -122,11 +123,6 @@ var BET_TYPES = {
   3: "Not Equal"
 };
 
-var COUNTER_BET = {
-  "Equal": 3,
-  "NotEqual": 2
-}
-
 var MAINNET_UNSPENDABLE = '1CounterpartyXXXXXXXXXXXXXXXUWLpVr';
 var TESTNET_UNSPENDABLE = 'mvCounterpartyXXXXXXXXXXXXXXW24Hef';
 var TESTNET_BURN_START = 154908;
@@ -151,8 +147,6 @@ var USE_TESTNET = (   ((location.pathname == "/" || location.pathname == "/src/"
                    || location.hostname.indexOf('testnet') == 0 ? true : false
                   );
 
-var TESTNET_PASSPHRASE = qs("passphrase");
-
 //CONSTANTS THAT DEPEND ON IS_DEV / USE_TESTNET
 var BLOCKEXPLORER_URL = USE_TESTNET ? "http://test.bitcore.io" : "http://live.bitcore.io";
 var GOOGLE_ANALYTICS_UAID = null; //will be set in counterwallet.js
@@ -160,8 +154,5 @@ var ROLLBAR_ACCESS_TOKEN = null; //will be set in counterwallet.js
 
 var TRANSACTION_DELAY = 5000 // delay between transaction to avoid error -22 (vin reused)
 var TRANSACTION_MAX_RETRY = 5 // max retry when transaction failed (don't include first transaction, so 3 retry means 4 queries)
-
-var FEED_CATEGORIES = ['sports', 'politics', 'entertainment', 'economics', 'other']
-var FEED_TYPES = ['simple', 'cfd']
 
 
